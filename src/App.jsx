642b4a4fef82a4f05b91e4c17ebb20e1b9c671ac
isMbumbu <andrewambuka9@gmail.com>
2025-02-27 06:47:36 +0300

    add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Navigation from './components/navigation'
+import { BrowserRouter} from 'react-router-dom'
 
 function App() {
 
-  return (
-        <h1>Moringa IMS</h1>
+  return (<>
+        <BrowserRouter>
+      <Navigation />
+      {/* Other components */}
+    </BrowserRouter>
+        <h1>Moringa IMS</h1></>
 
   )
 }
