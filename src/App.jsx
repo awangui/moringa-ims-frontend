cf0c9e2a7f64b49d77257e4c63b411cee3dbfda3
Anita Wangui <anitawangui22@gmail.com>
2025-03-04 12:51:34 +0300

    remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import './App.css'
 import MultiStepForm from './components/Vendors-form/Multiform';
 import Vendors from './pages/Vendors'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { VendorProvider } from './pages/VendorContext';
 import ViewVendor from './components/ViewVendor';
 import EditVendor from './components/EditVendor';
 import UploadDocuments from './components/UploadDocuments';
-import Navigation from './components/Navigation';
 
 function App() {
   return (
